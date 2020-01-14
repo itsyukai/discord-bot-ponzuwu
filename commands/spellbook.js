2e730f31aa2aeab28774e924f6ec7b688c449e09
itsyukai <itsyukai@gmail.com>
2020-01-14 14:27:54 -0800

    cleaned up reponse

--- commands/spellbook.js
+++ commands/spellbook.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: "spellbook",
-  aliases: ["s"],
+  aliases: ["s", "spell"],
   description: "Information on 5e spell",
+  usage: "<spell-name> [verbose]",
   args: true,
   execute(message, args) {
     var HttpClient = function() {
@@ -23,8 +24,50 @@ module.exports = {
     client.get(`http://www.dnd5eapi.co/api/spells/${args[0]}/`, function(
       response
     ) {
-      console.log(response);
-      message.channel.send(response);
+      res = JSON.parse(response);
+      if (res) {
+        if (args.length > 1) {
+          if (args[1] === "verbose" || args[1] === "v") {
+            // all spells have at least one class that uses it
+            var users = "";
+            for (var user in res.classes) {
+              users += res.classes[user].name + ", ";
+            }
+            users = users.slice(0, -2); // remove last 2 charactres for ", "
+
+            message.channel.send(
+              "```asciidoc\n" +
+                `= Spell Name: ${res.name} =\n` +
+                `[Description]\n${res.desc}\n` +
+                `[Higher Level]\n${res.higher_level}\n` +
+                `[Range]\n${res.range}\n` +
+                `[Components]\n${res.components}\n` +
+                `[Material]\n${res.material}\n` +
+                `[Casting Time]\n${res.casting_time}\n` +
+                `[Duration]\n${res.duration}\n` +
+                `[Concentration]\n${res.concentration}\n` +
+                `[Ritual]\n${res.ritual}\n` +
+                `[Classes]\n${users}\n` +
+                `[Page]\n${res.page}\n` +
+                "```"
+            );
+          }
+        } else {
+          message.channel.send(
+            "```asciidoc\n" +
+              `= Spell Name: ${res.name} =\n` +
+              `[Description]\n${res.desc}\n` +
+              `[Higher Level]\n${res.higher_level}\n` +
+              `[Range, Casting Time, Duration]\n${res.range}, ${res.casting_time}, ${res.duration}\n` +
+              `[Components]\n${res.components}\n` +
+              `[Concentration]\n${res.concentration}\n` +
+              `[Ritual]\n${res.ritual}\n` +
+              "```"
+          );
+        }
+      } else {
+        message.channel.send("Spell not found");
+      }
     });
   }
 };
